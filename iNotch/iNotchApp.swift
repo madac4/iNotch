3b05027f2158eeadb2b7b11914ee61dc07e01e0d
whhroo <orbupetru12@gmail.com>
2025-11-18 11:20:39 +0200

    Initial Commit

---
+++ iNotch/iNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNotchApp.swift
+//  iNotch
+//
+//  Created by Petru Orbu on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct iNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
